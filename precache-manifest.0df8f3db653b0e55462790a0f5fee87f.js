ae4e97c659dc76544e4063c6a5f233f8f4ff95e4
Saksham Kapur <saksham@sutratech.io>
2020-05-30 03:22:43 +0530

    Updates

---
+++ precache-manifest.0df8f3db653b0e55462790a0f5fee87f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df28f8c166d56dffeae71fb47de5b32",
+    "url": "/searchyie/index.html"
+  },
+  {
+    "revision": "eb7545a43dfed03c9b58",
+    "url": "/searchyie/static/css/main.80d5ea25.chunk.css"
+  },
+  {
+    "revision": "229e118fb70cf48b3bec",
+    "url": "/searchyie/static/js/2.c732df76.chunk.js"
+  },
+  {
+    "revision": "eb7545a43dfed03c9b58",
+    "url": "/searchyie/static/js/main.e85168d2.chunk.js"
+  },
+  {
+    "revision": "47c1e22e34621364ee3b",
+    "url": "/searchyie/static/js/runtime-main.0fcb4a54.js"
+  }
+]);
